9c6a3a9bac5ef9a473b079e621f9ea75e80e520c
jawadkhan4915-pro <jawad.khan4915@gmail.com>
2026-07-14 11:29:43 +0500

    feat: implement initial splash screen, main entry point, and platform channel navigation for power intercept handling

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:power_guard/main.dart';
